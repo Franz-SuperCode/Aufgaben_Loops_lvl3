9886089c13843501223f719cfc983097222b670b
Franz-SuperCode <franzsupercode@gmail.com>
2022-11-11 10:05:28 +0100

    fertig

--- lvl3_3/main.js
+++ lvl3_3/main.js
@@ -6,20 +6,24 @@ let output = document.querySelector("#output");
 button.addEventListener("click", () => {
 
     let input = Number(document.querySelector("#input").value);
-
-
-
     // Solange die Zahl vom Input <= 2 ist, soll ein "o" eingefügt werden
-    for (let i = 1; i <= input; i++) {
-
-        arrayLoop.splice(1, 0, "o");
-        if (input % 2 !== 0) {
-            arrayLoop.splice(1, 1, "0");
+    if (input % 2 === 0) {
+        for (let i = 0; i < input; i++) {
+            arrayLoop.splice(1, 0, "o");
         }
-
-
+    } else {
+        for (let i = 0; i < input; i++) {
+            //Bei jedem durchlauf der gerade ist ein "o". Bei ungerad eine "0"
+            if (i % 2 === 0) {
+                arrayLoop.splice(1, 0, "o");
+            } else {
+                arrayLoop.splice(1, 0, "0");
+            }
+        }
     }
-    // Das Array in einen Tring umwandeln
+
+
+    // Das Array in einen String umwandeln
     let arrayToString = arrayLoop.join("");
     // Den String der Methode übergeben
     write(arrayToString, input);
